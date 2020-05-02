ff96deaf1ff1768709be40828c264bf59f1c417a
John Ky <newhoggy@gmail.com>
2020-05-02 21:21:26 +1000

    Upgrade CI

--- project/Build.scala
+++ project/Build.scala
@@ -2,15 +2,15 @@ import sbt._
 import Keys._
 
 object Build {
-  val specs2_core       = "org.specs2"      %%  "specs2-core"         % "3.8.8"
-  val specs2_scalacheck = "org.specs2"      %%  "specs2-scalacheck"   % "3.8.8"
+  val specs2_core       = "org.specs2"      %%  "specs2-core"         % "4.8.3"
+  val specs2_scalacheck = "org.specs2"      %%  "specs2-scalacheck"   % "4.8.3"
   val scalacheck        = "org.scalacheck"  %%  "scalacheck"          % "1.13.4"
 
   implicit class ProjectOps(self: Project) {
     def standard: Project = {
       self
           .settings(organization := "org.picoworks")
-          .settings(resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases")
+          .settings(resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases")
           .settings(scalacOptions := Seq("-feature", "-deprecation", "-unchecked", "-Xlint", "-Yrangepos", "-encoding", "utf8"))
           .settings(scalacOptions in Test ++= Seq("-Yrangepos"))
     }
