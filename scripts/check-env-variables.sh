ad9a3d1d70efe17c122645d8301816d9363e1a8f
John Ky <newhoggy@gmail.com>
2020-05-02 21:21:26 +1000

    Upgrade CI

--- scripts/check-env-variables.sh
+++ scripts/check-env-variables.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-declare -a var_descriptions=(\
-"PUBLISH_ACCESS_KEY S3 access key for publishing" \
-"PUBLISH_SECRET_KEY S3 secret key for publishing" \
+declare -a vars=(\
+"AWS_ACCESS_KEY_ID      Access key for publishing to S3" \
+"AWS_SECRET_ACCESS_KEY  Secret key for publishing to S3" \
 )
 
-for var_description in "${var_descriptions[@]}"; do
-  var_name="${var_description%% *}"
-  if [ "${!var_name-x}" = "x" -a "${!var_name-y}" = "y" ]; then
-    echo -e "\x1B[31mError: Variable not defined: $var_description\x1B[0m"
+for var in "${vars[@]}"; do
+  v=${var%% *}
+  if [ "${!v-x}" = "x" -a "${!v-y}" = "y" ]; then
+    printf 'NOT FOUND: $%s\n' "$var"
     _not_found=true
   else
-    echo -e "\x1B[32mVariable defined: $var_description\x1B[0m"
+    printf 'found: $%s\n' "$v"
   fi
 done
 
-test -z "${_not_found}"
+[ -z "${_not_found}" ] && exit 0
+
+echo "*****************************"
+echo "MISSING ENVIRONMENT VARIABLES"
+echo "*****************************"
+exit 1
